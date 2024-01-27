57b0fe366a2d31d3569b42ef230d50a55eef6d0e
Rabindra Dhakal <contact@qaidvoid.dev>
2024-01-27 20:25:55 +0545

    test(init): setup integration tests

---
+++ server/tests/common/mod.rs
@@ -0,0 +1,53 @@
+use axum::Router;
+use contrx_server::{config::Config, database::Database, routes::create_router, AppState};
+
+use sqlx::PgPool;
+use uuid::Uuid;
+
+pub struct TestApp {
+    config: Config,
+    pool: PgPool,
+    pub app: Router,
+}
+
+impl TestApp {
+    pub async fn new() -> TestApp {
+        let mut config = Config::new();
+
+        let tmp = Database::new(&config.default_database_url()).await;
+        let pool = tmp.get_pool().await.clone();
+
+        config.database.name = Uuid::new_v4().to_string();
+        sqlx::query(&format!("CREATE DATABASE \"{}\"", config.database.name))
+            .execute(&pool)
+            .await
+            .unwrap();
+
+        let db = Database::new(&config.database_url()).await;
+        let pool = db.get_pool().await;
+
+        let state = AppState {
+            pool: pool.to_owned(),
+        };
+
+        let app = create_router(state);
+
+        TestApp {
+            config,
+            pool: pool.to_owned(),
+            app,
+        }
+    }
+
+    pub async fn drop_async(&self) {
+        self.pool.close().await;
+
+        let tmp = Database::new(&self.config.default_database_url()).await;
+        let pool = tmp.get_pool().await.clone();
+
+        sqlx::query(&format!("DROP DATABASE \"{}\"", &self.config.database.name))
+            .execute(&pool)
+            .await
+            .unwrap();
+    }
+}
---
+++ server/tests/test_health.rs
@@ -0,0 +1,30 @@
+use axum::{
+    body::Body,
+    http::{Request, StatusCode},
+};
+use tower::ServiceExt;
+
+use crate::common::TestApp;
+
+mod common;
+
+#[tokio::test]
+async fn health_should_be_ok() {
+    let t = TestApp::new().await;
+
+    let res = t
+        .app
+        .to_owned()
+        .oneshot(
+            Request::builder()
+                .uri("/health")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    t.drop_async().await;
+
+    assert_eq!(res.status(), StatusCode::OK);
+}
