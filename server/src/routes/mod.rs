e8e69371fe131e7b001c43c4fa49618b74efc011
Rabindra Dhakal <contact@qaidvoid.dev>
2024-05-01 21:55:41 +0545

    fix cors

--- server/src/routes/mod.rs
+++ server/src/routes/mod.rs
@@ -2,6 +2,7 @@ use std::time::Duration;
 
 use axum::{http::StatusCode, routing::get, Router};
 use tower_cookies::CookieManagerLayer;
+use tower_http::cors::{Any, CorsLayer};
 use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
 
 use crate::AppState;
@@ -20,12 +21,18 @@ async fn health() -> StatusCode {
 }
 
 pub fn create_router(state: AppState) -> Router {
+    let cors = CorsLayer::new()
+        .allow_headers(Any)
+        .allow_methods(Any)
+        .allow_origin(Any);
+
     Router::new()
         .route("/health", get(health))
         .nest("/api/auth", auth_router())
         .nest("/api/users", users_router())
         .fallback(not_found)
         .with_state(state)
+        .layer(cors)
         .layer((
             CookieManagerLayer::new(),
             TraceLayer::new_for_http(),
